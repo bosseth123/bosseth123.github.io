36e7440cfa9c5efbeccebbb75661670a5a00f405
bosseth123 <46276579+bosseth123@users.noreply.github.com>
2019-05-04 22:39:50 +0100

    d

--- js/javascript.js
+++ js/javascript.js
@@ -13,3 +13,22 @@ ScrollReveal().reveal('p', {delay:100, usedelay:"onload"});
 ScrollReveal().reveal('img', {delay:100, usedelay:"onload"});
 ScrollReveal().reveal('form', {delay:100, usedelay:"onload"});
 ScrollReveal().reveal('header img', {delay:0, usedelay:"onload"});
+
+
+
+$('.owl-carousel').owlCarousel({
+loop:true,
+margin:10,
+nav:true,
+responsive:{
+  0:{
+      items:1
+  },
+  600:{
+      items:3
+  },
+  1000:{
+      items:5
+  }
+}
+})
